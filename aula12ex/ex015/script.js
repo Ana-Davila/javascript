443968e7692983e32765a06dafe12b1f017d18a9
Ana Davila Kulek <anadavila.kulek@icloud.com>
2025-08-07 21:20:38 -0300

    script e style do verificador de idade.

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -14,11 +14,48 @@ function verificar(){
         var fsex = window.document.getElementsByName('radsex') //ByName porque existe mais de um que precisa ser selecionado
         var idade = ano - Number(fano.value)
         var gênero = ''
-        if(fsex[0].checked){
-            gênero = 'Homem'
+        var img = document.createElement('img') //cria um novo elemento img e guarda na var img
+        img.setAttribute('id', 'foto') //cria um id para imf chamado foto
+        if(fsex[0].checked){ // se fsexo[0] se fsexo for homem
+            gênero = 'Homem' //gênero= homem
+            if(idade >=0 && idade <10){ //idade entre 0 e 10
+                //criança
+                img.setAttribute('src', 'bebe-m.jpg')
+            }
+            else if(idade  < 21){
+                //jovem
+                img.setAttribute('src', 'jovem-m.jpg')
+            }
+            else if(idade < 50){
+                 //adulto
+                 img.setAttribute('src', 'adulto-m.jpg')
+            }
+            else {
+                //idoso
+                img.setAttribute('src', 'idoso-m.jpg')
+            }
         }
-        else if(fsex[1].checked){
-            gênero = 'Feminino'
+        else if(fsex[1].checked){ // senao se fsexo [1]= Feminino
+            gênero = 'Mulher' // gênero = Feminino
+            if(idade >=0 && idade <10){ //idade entre 0 e 10
+                //criança
+                img.setAttribute('src', 'bebe-f.jpg')
+            }
+            else if(idade  < 21){
+                //jovem
+                img.setAttribute('src', 'jovem-f.jpg')
+            }
+            else if(idade < 50){
+                 //adulto
+                 img.setAttribute('src', 'adulto-f.jpg')
+            }
+            else {
+                //idoso
+                img.setAttribute('src', 'idoso-f.jpg')
+            }
         }
+        res.style.textAlign='center'
+        res.innerHTML= `Detectamos um(a) ${gênero}, com ${idade} anos.`
+        res.append(img)
     }
 }
